6a7e67febce3acfaf81eeab6d43e9581d637b8c1
KhitishJena <satishkumarjena307@gmail.com>
2025-08-10 11:13:12 +0530

    maven clean and test runner updated

--- APIFramework/src/test/java/cucumber/Options/TestRunner.java
+++ APIFramework/src/test/java/cucumber/Options/TestRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/java/com/satish/Features",
 				glue = {"com.satish.StepDefinations"},
-				tags = "@AddPlace",
+				tags = "@End2End",
 				dryRun = false,
 				plugin = {"pretty", "html:target/cucumber-reports.html",
 						  "json:target/jsonReports/cucumber.json",
